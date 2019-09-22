aefd934e6e75a133521387bce15d909f5dbc8a0c
Nicolas <nicolas.delhomme@umu.se>
2019-09-22 16:12:53 +0200

    fixed a number of misconfigurations

--- src/bash/seidr-wrapper-kebnekaise.sh
+++ src/bash/seidr-wrapper-kebnekaise.sh
@@ -18,7 +18,21 @@ source /pfs/nobackup/home/b/bastian/seidr/build/sourcefile
 # Variables
 resultDir=results
 
+correlationNCPUs=28
+
 inference=(aracne clr genie3 llr-ensemble narromi pcor pearson plsnet spearman tigress)
+
+run=(
+  [0]=0
+  [1]=0
+  [2]=0
+  [3]=0
+  [4]=0
+  [5]=1
+  [6]=1
+  [7]=0
+  [8]=1
+  [9]=0)
 
 # 28 workers on 1 nodes (kk has 28 per node) - setting -n 2 -c 14 (14 cores on 2 nodes, results in the same)
 default="-n 1 -c 28 -t 1-00:00:00"
@@ -29,10 +43,10 @@ arguments=(
   [2]="-n 2 -c 28 -t 2-00:00:00"
   [3]=$default
   [4]=$default
-  [5]="-n 7 -t 12:00:00"
-  [6]="-n 7 -t 12:00:00"
+  [5]="-n 1 -c $correlationNCPUs -t 12:00:00"
+  [6]="-n 1 -c $correlationNCPUs -t 12:00:00"
   [7]=$default
-  [8]="-n 7 -t 12:00:00"
+  [8]="-n 1 -c $correlationNCPUs -t 12:00:00"
   [9]="-n 3 -c 28 -t 3-00:00:00")
 
 parallel="-O "'$SLURM_CPUS_PER_TASK'
@@ -110,10 +124,10 @@ ompThread=(
   [2]=$default
   [3]=$default
   [4]=$default
-  [5]=7
-  [6]=7
+  [5]=$correlationNCPUs
+  [6]=$correlationNCPUs
   [7]=$default
-  [8]=7
+  [8]=$correlationNCPUs
   [9]=$default)
 
 # Set dependencies
@@ -130,27 +144,28 @@ jobIDs=()
 len=${#inference[@]}
 for ((i=0;i<len;i++)); do
   inf=${inference[$i]}
-  if [ ! -f $resultDir/$inf/$inf.tsv ]; then
-    mkdir -p $resultDir/$inf
-    echo "#!/bin/bash" > $resultDir/$inf/$inf.sh
-    if [ -z ${ompThread[$i]} ]; then
-	echo "unset OMP_NUM_THREADS" >> $resultDir/$inf/$inf.sh
-    else
+  if [ ${run[$i]} -eq 1 ]; then
+    if [ ! -f $resultDir/$inf/$inf.tsv ]; then
+      mkdir -p $resultDir/$inf
+      echo "#!/bin/bash" > $resultDir/$inf/$inf.sh
+      if [ -z ${ompThread[$i]} ]; then
+ 	echo "unset OMP_NUM_THREADS" >> $resultDir/$inf/$inf.sh
+      else
     	echo "export OMP_NUM_THREADS=${ompThread[$i]}" >> $resultDir/$inf/$inf.sh
+      fi
+      echo "srun ${command[$i]} ${optionB[$i]} -i $1 -g $2 -o $resultDir/$inf/$inf.tsv" >> $resultDir/$inf/$inf.sh
+
+      # Handle dependencies
+      dep=
+      if [ ! -z ${deps[$i]} ]; then
+        dep="-d afterok:${jobIDs[${deps[$i]}]}"
+      fi
+
+      dep=$(sbatch --mail-type=ALL --mail-user=$mail -A $account -J $inf $dep \
+      -e $resultDir/$inf/$inf.err -o $resultDir/$inf/$inf.out ${arguments[$i]} $resultDir/$inf/$inf.sh)
+
+      jobIDs[$i]=${dep//[^0-9]/}
     fi
-    echo "srun ${command[$i]} ${optionB[$i]} -i $1 -g $2 -o $resultDir/$inf/$inf.tsv" >> $resultDir/$inf/$inf.sh
-
-    # Handle dependencies
-    dep=
-    if [ ! -z ${deps[$i]} ]; then
-      dep="-d afterok:${jobIDs[${deps[$i]}]}"
-    fi
-
-    dep=$(sbatch --mail-type=ALL --mail-user=$mail -A $account -J $inf $dep \
-    -e $resultDir/$inf/$inf.err -o $resultDir/$inf/$inf.out ${arguments[$i]} $resultDir/$inf/$inf.sh)
-
-    jobIDs[$i]=${dep//[^0-9]/}
-
   fi
 done
 
