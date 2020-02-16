fd71958d74273e1bd41493ed4464a396415a5494
Nicolas <nicolas.delhomme@umu.se>
2020-02-16 13:57:55 +0100

    Added a cat job to combine the el-ensemble output into elnet

--- src/bash/seidr-import-kebnekaise.sh
+++ src/bash/seidr-import-kebnekaise.sh
@@ -17,16 +17,17 @@ source /pfs/nobackup/home/b/bastian/seidr/build/sourcefile
 inference=(aracne clr elnet genie3 llr-ensemble mi narromi pcor pearson plsnet spearman tigress)
 
 # additional parameters (elnet is done iteratively, so the format is not the expected one: a matrix, rather an edge list
-arguments=([2]="-f el" [4]="-o llr-ensemble.sf")
+arguments=([2]="-f el" [4]="-o results/llr-ensemble/llr-ensemble.sf")
 CPUs=28
 Time=1-00:00:00
+ShortTime=1:00:00
 
 # usage
 USAGETXT=\
 "
 $0 <genes.tsv>
 
-Methods to be import from: ${inference[@]}
+Methods to be imported from: ${inference[@]}
 
 "
 
@@ -40,19 +41,41 @@ if [ ! -f $1 ]; then
 fi
 
 # Create template script and submit
+jobID=
 len=${#inference[@]}
 for ((i=0;i<len;i++)); do
   inf=${inference[$i]}
+  if [ "$inf" == "elnet" ]; then
+    # create a job to cat el-ensemble into elnet
+    if [ ! -d results/$inf ]; then
+	mkdir -p results/$inf
+      echo "#!/bin/bash" > results/$inf/cat.sh
+      echo "cat results/el-ensemble/*.tsv > results/$inf/$inf.tsv ">> results/$inf/cat.sh
+      dep=$(sbatch -t $ShortTime -c 1  --mail-type=ALL --mail-user=$mail -A $account -J $inf-cat \
+      -e results/$inf/$inf-$i.err -o results/$inf/$inf-$i.out \
+      results/$inf/cat.sh)
+      jobID="-d afterok:${dep//[^0-9]/}"
+    fi
+  fi
   if [ ! -f results/$inf/$inf.sf ]; then
-
     if [ -f results/$inf/$inf.tsv ]; then
-      /pfs/nobackup/home/d/delhomme/seidr/scripts/generate_import_script.py \
+      ./generate_import_script.py \
       -i results/$inf/$inf.tsv -g $1 -c $CPUs ${arguments[$i]} > results/$inf/${inf}-import.sh
 
-      sbatch -t $Time --mail-type=ALL --mail-user=$mail -A $account -J import-$inf \
+      sbatch -t $Time --mail-type=ALL --mail-user=$mail -A $account -J import-$inf $jobID\
 	-e results/$inf/${inf}-import.err -o results/$inf/${inf}-import.out results/$inf/${inf}-import.sh
     else
-      echo "There is no tsv file for $inf"
+      if [ "$jobID" != "" ]; then
+        # most likely the tsv file for elnet won't exist, so give it a chance (as there is a dep)
+	# we touch the file so the generate import script does not fail
+	touch results/$inf/$inf.tsv
+        ./generate_import_script.py \
+        -i results/$inf/$inf.tsv -g $1 -c $CPUs ${arguments[$i]} > results/$inf/${inf}-import.sh
+        sbatch -t $Time --mail-type=ALL --mail-user=$mail -A $account -J import-$inf $jobID\
+	      -e results/$inf/${inf}-import.err -o results/$inf/${inf}-import.out results/$inf/${inf}-import.sh
+      else
+        echo "There is no tsv file for $inf"
+      fi
     fi
 
     # ${arguments[$i]}
