a35cf7297dd172e1434692f5cbb8d14e9332da85
Nicolas <nicolas.delhomme@umu.se>
2020-03-17 16:50:14 +0100

    Added a data.frame dispatch

--- src/R/volcanoPlot.R
+++ src/R/volcanoPlot.R
@@ -67,3 +67,36 @@ setMethod(f="volcanoPlot",
             ## circle the points for the dot plot
             abline(h=-log10(alpha),lty=2,lwd=2,col="gray")
           })
+
+setMethod(f="volcanoPlot",
+          signature="data.frame",
+          definition=function(object,alpha=0.01,lfc=0.5){
+            
+            ## lib
+            require(LSD)
+            
+            ## selectors
+            sel <- ! is.na(object$adj.P.Val)
+            sel2 <- object$adj.P.Val[sel]<=alpha & 
+              abs(object$logFC[sel]) >= lfc
+            
+            ## plot
+            heatscatter(object$logFC[sel],
+                        -log10(object$adj.P.Val[sel]),
+                        main="Volcano plot",xlab="log2 Fold Change",
+                        ylab="- log(10) BH adjusted p-value")
+            
+            ## legend
+            legend("top",bty="n",paste("cutoff @",alpha,"and",lfc),lty=2,lwd=2,col="gray")
+            
+            ## points
+            points(object$logFC[sel][sel2],-log10(object$adj.P.Val[sel][sel2]),col="lightblue",pch=19)
+            points(object$logFC[sel][sel2],-log10(object$adj.P.Val[sel][sel2]),col="dodgerblue3",pch=19,cex=0.5)
+            
+            ## circle the points for the dot plot
+            abline(h=-log10(alpha),lty=2,lwd=2,col="gray")
+            if(lfc != 0) {
+              abline(v=lfc*c(-1,1),lty=2,lwd=2,col="gray")
+            }
+          })
+
