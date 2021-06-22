c243cf7bbf951544a46317658c70ac72516fac98
Nicolas <nicolas.delhomme@umu.se>
2021-06-22 11:26:45 +0200

    Added an option for the counter length

--- pipeline/runJellyfishCount.sh
+++ pipeline/runJellyfishCount.sh
@@ -16,25 +16,28 @@ KMERSIZE=25
 HASHSIZE=100G
 CPUS=12
 BC=
+OUTCL=4
 
 ## usage
 USAGETXT=\
 "
-	Usage: runJellyfishBc.sh <in.bam> <out.dir>
+	Usage: runJellyfishBc.sh [options] <in.bam> <out.dir>
 
 	Options:
 	  -b the path to a bc file
 	  -C turn off canonical mode (on by default)
+	  -l set --out-counter-len (default 4, can be set to 2 or 1 to reduce mem usage)
 	  -m kmer size (default $KMERSIZE)
 	  -s hash size (defaut $HASHSIZE)
 	  -t threads (default $CPUS)
 "
 
-while getopts b:Cm:s:t: option
+while getopts b:Cl:m:s:t: option
 do
         case "$option" in
         b) BC=$OPTARG;;
         C) CANON="";;
+        l) OUTCL=$OPTARG;;
 	      m) KMERSIZE=$OPTARG;;
         s) HASHSIZE=$OPTARG;;
         t) CPUS=$OPTARG;;
@@ -53,8 +56,11 @@ shift `expr $OPTIND - 1`
 
 [[ ! -z $BC ]] && BC="--bc $BC"
 
+[[ $OUTCL -ne 1 ]] || [[ $OUTCL -ne 2 ]] || [[ $OUTCL -ne 4 ]] && abort "-l must be a value among 1,2 or 4"
+
 isExec jellyfish
 isExec samtools
 
-jellyfish count -m $KMERSIZE -s $HASHSIZE $CANON -o $2/$(basename ${1/.bam/.jf}) -t $CPUS $BC <(samtools view $1 | awk '{print ">"$1"\n"$10}')
+jellyfish count -m $KMERSIZE -s $HASHSIZE $CANON \
+-o $2/$(basename ${1/.bam/.jf}) -t $CPUS $BC --out-counter-len $OUTCL <(samtools view $1 | awk '{print ">"$1"\n"$10}')
 
