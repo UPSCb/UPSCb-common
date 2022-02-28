1e6d6e1490ffdd111d28692b0cb0b4b54097fe0d
Nicolas <nicolas.delhomme@umu.se>
2022-02-28 21:21:50 +0100

    Updated the runDigitalNormalization.sh script

--- pipeline/runDigitalNormalization.sh
+++ pipeline/runDigitalNormalization.sh
@@ -10,14 +10,10 @@
 ## mail-user and A have to be set in the submit script
 
 ## stop on error
-set -e
+set -eu
 
 ## be verbose and extend the commands
 set -x
-
-## load module
-module load bioinfo-tools
-module load trinity
 
 ## check the options if any
 MEM=40G
@@ -25,12 +21,14 @@ KMER=200
 PROC=20
 SINGLE=0
 
+## source functions
+source ${SLURM_SUBMIT_DIR:-$(pwd)}/../UPSCb-common/src/bash/functions.sh
+
 ## usage
-usage(){
-echo >&2 \
+USAGETXT=\
 "
-	Usage: runDigitalNormalization.sh [options] <left fq> <right fq> <out dir> [trinity options]
-	runDigitalNormalization.sh [options] -s <single fq> <out dir> [trinity options]
+	Usage: runDigitalNormalization.sh [options] <singularity container> <left fq> <right fq> <out dir> [trinity options]
+	runDigitalNormalization.sh [options] -s <singularity container> <single fq> <out dir> [trinity options]
 
 	Options:
                 -k max kmer cov (default 200)
@@ -41,8 +39,6 @@ echo >&2 \
         Note:
              <left fq> and <right fq> could also be files containing a list of filenames (one per line)
 "
-	exit 1
-}
 
 ## get the options
 while getopts k:m:p:s option
@@ -53,54 +49,40 @@ do
 	    p) PROC=$OPTARG;;
 	    s) SINGLE=1;;
 		\?) ## unknown flag
-		usage;;
+		abort "Unknown option";;
         esac
 done
 shift `expr $OPTIND - 1`
 
 ## we get two dir and two files as input
 if [ $SINGLE -eq 0 ]; then
-    if [ $# -lt 3 ]; then
-	echo "This function takes two files and one directory as arguments for PE data"
-	usage
-    fi
+  [[ $# -lt 4 ]] && abort "This function takes three files and one directory as arguments for PE data"
 else 
-    if [ $# -lt 2 ]; then
-	echo "This function takes one files and one directory as arguments for SE data"
-	usage
-    fi
+  [[ $# -lt 3 ]] && abort "This function takes two files and one directory as arguments for SE data"
 fi
+
+singularity=$1
+shift
+[[ ! -f $singularity ]] && abort "The first argument needs to be the trinity singularity container file"
 
 fwd=$1
 shift
-if [ ! -f $fwd ]; then
-    echo "The first argument needs to be the forward (left) fastq file"
-    usage
-fi
+[[ ! -f $fwd ]] && abort "The second argument needs to be the forward (left) fastq file"
 
 if [ $SINGLE -eq 0 ]; then
     rev=$1
     shift
-    if [ ! -f $rev ]; then
-	echo "The second argument needs to be the reverse (right) fastq file"
-	usage
-    fi
+    [[ ! -f $rev ]] && abort "The third argument needs to be the reverse (right) fastq file"
 else
     dir=$1
     shift
-    if [ ! -d $dir ]; then
-	echo "The second argument (output dir) needs to be an existing directory"
-	usage
-    fi
+    [[ ! -d $dir ]] && abort "The third argument (output dir) needs to be an existing directory"
 fi
 
 if [ $SINGLE -eq 0 ]; then
     dir=$1
     shift
-    if [ ! -d $dir ]; then
-	echo "The third argument (output dir) needs to be an existing directory"
-	usage
-    fi
+    [[ ! -d $dir ]] &&abort  "The forth argument (output dir) needs to be an existing directory"
 fi
 
 ## do we have file lists
@@ -110,22 +92,23 @@ else
 	firstLine=$(head -1 $fwd)
 	LIST=""
 	if [ -f $firstLine ]; then
-	LIST="_list"
-	if [ $SINGLE -ne 0 ]; then
-		# we concatenate instead
-		for f in $(cat $fwd); do
-			l=${l:+$l,}$f
-		done
-		fwd=$l
-	fi
+	  LIST="_list"
+	  if [ $SINGLE -ne 0 ]; then
+		  # we concatenate instead
+		  for f in $(cat $fwd); do
+			  l=${l:+$l,}$f
+		  done
+		  fwd=$l
+	  fi
 	fi
 fi
 
 ## run trinity
 if [ $SINGLE -eq 0 ]; then
-	singularity exec --bind /mnt:/mnt /mnt/picea/projects/singularity/trinity-2.8.5.simg \
-	/usr/local/bin/trinityrnaseq/util/insilico_read_normalization.pl --seqType fq --JM $MEM --left$LIST $fwd --right$LIST $rev --output $dir --CPU $PROC --max_cov $KMER --pairs_together $@
+	singularity exec $singularity \
+	/usr/local/bin/util/insilico_read_normalization.pl --seqType fq --JM $MEM --left$LIST $fwd --right$LIST $rev --output $dir --CPU $PROC --max_cov $KMER --pairs_together $@
 else
-    $TRINITY_HOME/util/insilico_read_normalization.pl --seqType fq --JM $MEM --single $fwd --output $dir --CPU $PROC --max_cov $KMER $@
+  singularity exec $singularity \
+	/usr/local/bin/util/insilico_read_normalization.pl --seqType fq --JM $MEM --single $fwd --output $dir --CPU $PROC --max_cov $KMER $@
 fi
 
