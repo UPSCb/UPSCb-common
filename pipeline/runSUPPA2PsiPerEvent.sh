d1419e744795c4fa67df56c1e288745b9bcef3b3
Nicolas <nicolas.delhomme@umu.se>
2022-09-16 10:56:24 +0200

    Extended to ignore empty.html Added a SUPPA2 script to run psiPerEvent

---
+++ pipeline/runSUPPA2PsiPerEvent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#SBATCH -p core -n 1
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name="suppa2"
+#SBATCH --mail-type=END,FAIL
+
+# sanity
+set -eu
+
+# functions
+source ${SLURM_SUBMIT_DIR:-$(pwd)}/../UPSCb-common/src/bash/functions.sh
+
+# usage
+USAGETXT=\
+"
+  Synopsis [options] $0 <suppa2 singularity container> <ioe file> <count tsv> <out dir>
+"
+
+# checks
+[[ ! $@ -ne 4 ]] && abort "This script expects three arguments"
+[[ ! -f $1 ]] && abort "The first argument needs to be an existing file path to a singularity container"
+[[ -z ${SINGULARITY_BINDPATH:-} ]] && abort "This function relies on singularity, set the SINGULARITY_BINDPATH environment variable"
+[[ ! -f $2 ]] && abort "The second argument needs to be an existing file path to an ioe annotation file"
+[[ ! -f $3 ]] && abort "The third argument needs to be an existing file path to a count tsv file"
+[[ ! -d $4 ]] && abort "The fourth argument needs to be an existing directory"
+
+# run
+singularity exec $1 \
+suppa.py psiPerEvent \
+-i $2 \
+-o $4/$(basename ${3/.tsv/})_$(basename ${2/.ioe/} \
+-e $3
